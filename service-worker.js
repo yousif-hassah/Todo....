fa9e87fd0efbd94e4549ac5dee2f4f70bd071964
yousif <yousifhassan4247@gmail.com>
2025-07-28 08:03:24 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("todo-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/second.css",
+        "/alarm_clock.mp3",
+        "/image/icon.jpeg",
+        "/image/alarm.jpeg",
+        "/image/todo1.jpeg",
+        "/image/checked.jpeg",
+        "/image/uncheked.jpg",
+        "/image/delete-icon.jpeg" ,
+        "/icons/icon-192.png",
+        "/icons/icon-512.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
